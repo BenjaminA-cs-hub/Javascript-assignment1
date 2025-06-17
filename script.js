89313fe334ad76dbd9cd3f74ef2410d9e2aaf6e1
PKhant78 <phonekhantkyawswa555@gmail.com>
2025-06-17 14:48:16 -0400

    Implemented myLastIndexOf function

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// 6
 function myIncludes(arr, target) {
     for (let i = 0; i < arr.length; i++) {
         if (arr[i] === target) {
@@ -10,6 +11,7 @@ function myIncludes(arr, target) {
 console.log("Myincludes function test: ")
 console.log(myIncludes(["cat", "dog", "cow"], "cat"));
 
+// 7
 function myIndexOf(arr, target) {
     for (let i = 0; i < arr.length; i++) {
         if (arr[i] === target) {
@@ -22,7 +24,7 @@ function myIndexOf(arr, target) {
 console.log("myIndexOf() function test: ")
 console.log(myIndexOf(["cat", "dog", "cow"], "dog"));
 
-
+// 8
 const testArray = ["boat", "car", "plane"];
 
 function myPush(arr, item) {
@@ -32,3 +34,18 @@ function myPush(arr, item) {
 console.log("myPush() function test: ")
 console.log("Adding tank to " + testArray);
 console.log(myPush(testArray, "tank"));
+
+// 9
+function myLastIndexOf(arr, target) {
+    for (let i = arr.length - 1; i >= 0; i--) {
+        if (arr[i] === target) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+const numArray = [1,2,3,4,10,12];
+console.log("myLastIndexOf() function test: ")
+console.log("Finding index of 10 in " + numArray);
+console.log(myLastIndexOf(numArray, 10));
